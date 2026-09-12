4e76683b6bb36f14153e96b2c08e14510d3e681e
z7zt8z8w7t-ops <z7zt8z8w7t@privaterelay.appleid.com>
2026-09-12 21:30:12 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='couples-after-dark-v16-no-reveal-chime';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./click.wav','./chime.wav','./spin.wav'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
