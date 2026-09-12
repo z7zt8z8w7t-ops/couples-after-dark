466801d09037734ba4e0c98283213cb43bb316d0
z7zt8z8w7t-ops <z7zt8z8w7t@privaterelay.appleid.com>
2026-09-12 21:09:34 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='couples-after-dark-v10-fully-verified';
+const CACHE='couples-after-dark-v12-final-function-tested';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
